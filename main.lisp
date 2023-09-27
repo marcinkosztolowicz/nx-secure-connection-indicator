4aa0fb99d4a472f974dee0999cc781cf35194306
kostek <realpossiblestudio@gmail.com>
2023-09-27 17:38:52 +0200

    imporvements

--- main.lisp
+++ main.lisp
@@ -3,52 +3,57 @@
 (nyxt:define-package :nx-secure-connection-indicator)
 
 (in-package #:nyxt)
- ;set parameter in plist checkout (inspect 'nyxt::format-status-load-status)
-(if (eq (get 'nyxt::format-status-load-status 'nyxt::nyxt-status-default-method-definition) 'nil)
-    (setf (get 'nyxt::format-status-load-status 'nyxt::nyxt-status-default-method-definition) (fdefinition 'nyxt::format-status-load-status))
+ ;set parameter in plist checkout (inspect 'nyxt::format-status-buttons)
+(if (eq (get 'nyxt::format-status-buttons 'nyxt::nyxt-status-default-method-definition) 'nil)
+    (setf (get 'nyxt::format-status-buttons 'nyxt::nyxt-status-default-method-definition) (fdefinition 'nyxt::format-status-buttons))
     )
  ; test in slime repl (format-status-load-status (status-buffer (current-window)))
  ; old working version for 3.0.0
-(defun format-status-load-status (status)
-  (let ((x (get 'nyxt::format-status-load-status 'nyxt::nyxt-status-default-method-definition))
+(defun format-status-buttons (status)
+  (let ((format-status-buttons-default (get 'nyxt::format-status-buttons 'nyxt::nyxt-status-default-method-definition))
         (buffer (current-buffer (window status))))
-    (concatenate 'string (funcall x status) (nx-secure-connection-indicator::tlshtmlbutton (nx-secure-connection-indicator::tlsstr buffer) status) )))
-
-;; (defun format-status-load-status (status)
-;;   (nx-secure-connection-indicator::mytest))
-
-;; (defun format-status-load-status (status)
-;;   (let ((x (get 'nyxt::format-status-load-status 'nyxt::nyxt-status-default-method-definition))
-;;         (buffer (current-buffer (window status))))
-;;     (write-to-string (nx-secure-connection-indicator::tlsstr buffer))))
-
-
+    (format nil "~a~%  ~a" (funcall format-status-buttons-default status)  (nx-secure-connection-indicator::tlshtmlbutton (nx-secure-connection-indicator::tlsstr buffer) status)   )))
 
 
 (in-package :nx-secure-connection-indicator)
 
 
-(defun mytest ()
-  "LOL2")
+(defclass raw-tls-info ()
+  ((is-using-https
+    :initarg :is-using-https
+    :accessor is-using-https)
+   (certificate-flags-int
+    :initarg :certificate-flags-int
+    :accessor certificate-flags-int)))
 
-;(in-package #:webkit2)
-(defun tlspointer (my-ptr-to-int buffer)
+(defclass tls-info-render ()
+  ((secure-symbol
+    :initarg :secure-symbol
+    :accessor secure-symbol)
+   (color
+    :initarg :color
+    :accessor color)
+   (text-info
+    :initarg :text-info
+    :accessor text-info)
+   ))
+
+
+;https://webkitgtk.org/reference/webkit2gtk/2.5.1/WebKitWebView.html
+;https://api.gtkd.org/gio.c.types.GTlsCertificateFlags.html
+(defun get-tls-info-with-pointer (my-ptr-to-int buffer)
   ""
   (let ((result (webkit:webkit-web-view-get-tls-info (NYXT/RENDERER/GTK:GTK-OBJECT buffer) (cffi:null-pointer) my-ptr-to-int)))
-    (list result (cffi:mem-aref my-ptr-to-int :int)))
+    (make-instance 'raw-tls-info
+     :is-using-https result
+     :certificate-flags-int (cffi:mem-aref my-ptr-to-int :int)))
   )
 
 
-
-;;(nyxt::status buffer)
-;(nyxt::status buffer)
-;;;(nyxt::status (current-buffer))
-
-; list '(T/NIl  flag_value)
-(defun tls (buffer)
+(defun get-tls-info (buffer)
   ""
   (cffi:with-foreign-object (pointer :int 1)
-    (tlspointer pointer buffer)
+    (get-tls-info-with-pointer pointer buffer)
     )
   )
 
@@ -62,46 +67,57 @@
   (if (and (typep buffer 'nyxt::web-buffer) (eq (nyxt::status buffer) :finished))
 
       (let
-          ((result (tls buffer)))
-        (list (format nil "[~a]"(cert-status-symbol result)) (format nil "~a" (cert-status-info result)))
+          ((raw-tls-info-value (get-tls-info buffer)))
+        (map-tls-info-render raw-tls-info-value)
         )
 
-      '("?" "certificate satatus is unknown"))
-  )
-
-(defun cert-status-symbol (result)
-  ""
-  (if (eq (nth 0 result) 'nil)
-      "X"                               ;http , lock open
-      (if (= (nth 1 result) 0)
-          "S"                           ; secure https
-          "⚠"                           ; unsecure certifcate
-          )
-      )
-  )
-
-(defun cert-status-info (result)
-  ""
-  (if (eq (nth 0 result) 'nil)
-      "not secure connection"                               ;http , lock open
-      (if (= (nth 1 result) 0)
-          "secure connection"                           ; secure https
-          (format nil "certificate issue: ~a" (list-of-bits (nth 1 result))) ; unsecure certifcate
-          )
+      (make-instance 'tls-info-render
+                     :secure-symbol "?"
+                     :color "#888888"
+                     :text-info "certificate satatus is unknown"
+                     )
       )
   )
 
 
+(defun map-tls-info-render (raw-tls-info-value)
+  ""
+  (cond
+    ((eq (is-using-https raw-tls-info-value) 'nil)
+     (make-instance 'tls-info-render
+                     :secure-symbol "X"
+                     :color "#f9c80e"
+                     :text-info "HTTP (not secure)") )
+    ((= (certificate-flags-int raw-tls-info-value) 0)
+     (make-instance 'tls-info-render
+                     :secure-symbol "S"
+                     :color "#43bccd"
+                     :text-info "HTTPS ( secure)"))
+    (t
+     ( make-instance 'tls-info-render
+                     :secure-symbol "⚠"
+                     :color "#ea3546"
+                     :text-info (format nil "https certificate issue: ~a" (list-of-bits (certificate-flags-int raw-tls-info-value))))))
 
-(defun tlshtmlbutton (strings status)
+  )
+
+
+(defun tlshtmlbutton (tls-info-render-value status)
   ""
   (spinneret:with-html-string
-      (:nbutton
-       :buffer status
-       :text (nth 0 strings)
-       :title (nth 1 strings)
-       :style "background-color:#4CAF50;" ;"background-color:#4CAF50;"
-       '(nyxt:set-url))))
+      (:div
+       :id "tls"
+       :style "float:right;margin-right: +10px;"
+       (:nbutton
+
+        :buffer status
+        :text (format nil "[~a]" (secure-symbol tls-info-render-value))
+
+        :title (text-info tls-info-render-value)
+        :style (format nil "background-color:~a;color: black;width: 22px" (color tls-info-render-value)) ; "background-color:#4CAF50;"
+        '(nyxt:set-url)))))
+
+
 
 
 
